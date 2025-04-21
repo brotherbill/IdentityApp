0bafb8ec0d805556a776d2eb9c3577d7406816f1
Brother Bill Steinberg <brotherbill@mail.com>
2025-04-20 20:50:25 -0400

    end 2.7 Create JWT Service

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,5 +1,7 @@
 using Api.Data;
+using Api.Models;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,6 +19,28 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<Context>(options => {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+// be able to inject JWTService class inside our Controllers
+builder.Services.AddScoped<JwtScoped>();
+
+// defining our IdentityCore Service
+builder.Services.AddIdentityCore<User>(options => {
+        // password configuration
+        options.Password.RequiredLength = 6;
+        options.Password.RequireDigit = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+
+        // for email confirmation
+        options.SignIn.RequireConfirmedAccount = true;
+    })
+    .AddRoles<IdentityRole>()                       // be able to add roles
+    .AddRoleManager<RoleManager<IdentityRole>>()    // be able to make use of RoleManager
+    .AddEntityFrameworkStores<Context>()            // provide the DbContext to Identity
+    .AddSignInManager<SignInManager<User>>()        // provide the SignInManager to Identity
+    .AddUserManager<UserManager<User>>()            // make use of UserManager to create users
+    .AddDefaultTokenProviders();                    // be able to create tokens for email confirmation
 
 var app = builder.Build();
 
